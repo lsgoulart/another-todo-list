1f1f922123412d5f209b374e93176b8342c87f8a
Leonardo Goulart <leonardo.sgoulart@gmail.com>
2016-05-20 03:37:16 -0300

    adding build

--- server/server.js
+++ server/server.js
@@ -1,23 +1,26 @@
 var express = require('express');
 var path = require('path');
 
-var config = require('../webpack.config.js');
-var webpack = require('webpack');
-var webpackDevMiddleware = require('webpack-dev-middleware');
-var webpackHotMiddleware = require('webpack-hot-middleware');
-var compiler = webpack(config);
-
 var app = express();
 
-app.use(express.static('./dist'));
+if (process.env.NODE_ENV !== 'production') {
 
-app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
-app.use(webpackHotMiddleware(compiler));
+    var config = require('../webpack.config.js');
+    var webpack = require('webpack');
+    var webpackDevMiddleware = require('webpack-dev-middleware');
+    var webpackHotMiddleware = require('webpack-hot-middleware');
+    var compiler = webpack(config);
+
+    app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
+    app.use(webpackHotMiddleware(compiler));
+}
 
 app.use('/', function (req, res) {
     res.sendFile(path.resolve('client/index.html'));
 });
 
+
+app.use(express.static('./dist'));
 var port = 3000;
 
 app.listen(port, function(error) {
---
+++ webpack.prod.config.js
@@ -0,0 +1,47 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: [
+        './client/client.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+
+    plugins: [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ],
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['react', 'es2015', 'react-hmre']
+                }
+            },
+
+            {
+                test: /\.scss$/,
+                loaders: ["style", "css", "sass"]
+            }
+        ]
+    }
+}
